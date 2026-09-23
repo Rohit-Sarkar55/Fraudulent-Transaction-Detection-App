b0ebda19daca149b470de875adcd12c3e7fe4506
Rohit-Sarkar55 <sarkarrohit650@gmail.com>
2026-09-23 10:36:15 +0530

    main: enable scheduling

--- src/main/java/com/confluent/frauddetectionapp/FraudDetectionAppApplication.java
+++ src/main/java/com/confluent/frauddetectionapp/FraudDetectionAppApplication.java
@@ -2,8 +2,10 @@ package com.confluent.frauddetectionapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FraudDetectionAppApplication {
 
     public static void main(String[] args) {
